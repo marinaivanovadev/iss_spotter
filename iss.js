2be96e284021e6b06353ffbc1d84f1f0a621ca61
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-06-17 21:10:11 -0700

    Add error, statusCode

--- iss.js
+++ iss.js
@@ -14,7 +14,14 @@ const fetchMyIP = function(callback) {
   request('https://api.ipify.org?format=json', (error, response, body) => {
     if (error) {
       callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
     } else {
+
       const data = JSON.parse(body);
       const ip = data.ip;
       callback(null, ip);
