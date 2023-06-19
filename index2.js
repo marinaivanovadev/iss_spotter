ead5015a6aa42461969bb7e0959a91311570198e
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-06-18 21:17:53 -0700

    3 functions promised

--- index2.js
+++ index2.js
@@ -2,7 +2,12 @@
 // call .then on its return value.
 // This then call should take in a callback which accepts the response body and, for now, prints it to the screen.
 
-const { fetchMyIP } = require('./iss_promised');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes  } = require('./iss_promised');
 
 fetchMyIP()
-  .then(ip => console.log(ip));
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then(body => console.log(body));
+
+
+
--- iss_promised.js
+++ iss_promised.js
@@ -4,4 +4,16 @@ const fetchMyIP = function() {
   return request('https://api.ipify.org?format=json');
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://ipwho.is/${ip}`);
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  const {latitude, longitude } = JSON.parse(body);
+
+  return request(`https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`);
+  
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
