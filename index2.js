ef034d59a05ed06dc2321fb577d06957ef333d66
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-06-18 20:56:17 -0700

    fetchMyIP

---
+++ index2.js
@@ -0,0 +1,8 @@
+//Require and call the function fetchMyIP in this file.
+// call .then on its return value.
+// This then call should take in a callback which accepts the response body and, for now, prints it to the screen.
+
+const { fetchMyIP } = require('./iss_promised');
+
+fetchMyIP()
+  .then(ip => console.log(ip));
